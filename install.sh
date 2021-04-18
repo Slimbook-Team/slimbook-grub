eb87127edc7f1861913192b007b8c3e698f4fe34
SLIMBOOK TEAM <sat@slimbook.es>
2021-04-18 20:26:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ do
     count=$((count+1))
 done
 
-if [ $CONTINUE != 'Y' ] || [ $CONTINUE = 'y' ]
+if [ $CONTINUE != 'Y' ] || [ $CONTINUE != 'y' ]
 then
     echo $installatin_canceled_msg
     exit 0
